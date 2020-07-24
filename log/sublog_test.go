8021604527f3e456f8a93368fd903652fd3a0a9a
ardit <ardit@ardit2.local>
2020-07-24 14:44:25 +0700

    removing file writer on test

--- log/sublog_test.go
+++ log/sublog_test.go
@@ -3,7 +3,6 @@ package log
 import (
 	"context"
 	"errors"
-	"os"
 	"testing"
 
 	"github.com/kitabisa/perkakas/v2/internal"
@@ -11,18 +10,11 @@ import (
 
 func TestSublogger(t *testing.T) {
 	ctx := context.WithValue(context.Background(), internal.CtxXKtbsRequestID, "111111")
-	f, err := os.Create("logfile-test.log")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer f.Close()
 
 	subLog := GetSublogger(ctx, "test-context-name-1")
-	subLog.Output(f)
 	subLog.Err(errors.New("test-error")).Msg("test-message")
 
 	subLog2 := GetSublogger(ctx, "test-context-name-2")
-	subLog2.Output(f)
 	subLog2.Info().Msg("test-message")
 
 	// as log is not printed when test is success, need to uncomment line below :D
