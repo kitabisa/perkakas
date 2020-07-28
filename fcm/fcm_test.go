eba434411b1053a46b81134d8d17993c6813701b
Budi Ariyanto <budi@kitabisa.com>
2020-07-28 15:56:36 +0700

    Fix unit test

--- fcm/fcm_test.go
+++ fcm/fcm_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-func TestSendMessage(t *testing.T) {
-	f, err := NewFcm("path_to_key_file.json")
+func TestSendWithoutNotification(t *testing.T) {
+	f, err := NewFcm("path_to_key.json")
 	if err != nil {
 		t.Fail()
 	}
@@ -15,7 +15,23 @@ func TestSendMessage(t *testing.T) {
 		"msg": "perkakas test send message to fcm",
 	}
 
-	_, err = f.Send(context.Background(), "topic_test", data)
+	_, err = f.SendWithoutNotification(context.Background(), "topic_test", data)
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestSendWithNotification(t *testing.T) {
+	f, err := NewFcm("path_to_key.json")
+	if err != nil {
+		t.Fail()
+	}
+
+	data := map[string]string {
+		"msg": "perkakas test send message to fcm",
+	}
+
+	_, err = f.Send(context.Background(), "topic_test", "this is title notification", "this is the body", "https://image.com/location/img.jpg", data)
 	if err != nil {
 		t.Fail()
 	}
