b2d6f35151bfd68abc5fa44dcf723ce21a978049
Budi Ariyanto <budi@kitabisa.com>
2020-10-06 18:01:49 +0700

    Unit test using assert

--- text/formatter_test.go
+++ text/formatter_test.go
@@ -1,19 +1,18 @@
 package text
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
 )
 
 func TestFormatCurrency(t *testing.T) {
-	FormatIDR(1000000)
-
-	// Output: Rp1.000.000
+	amountIDR := FormatIDR(1000000)
+	assert.Equal(t, "Rp1.000.000", amountIDR)
 }
 
 func TestTranslateDateToIndonesian(t *testing.T) {
 	date := time.Date(2020, time.October, 6, 10, 46, 03, 0, time.UTC)
-	TranslateDateToIndonesian(date)
-
-	// Output: Selasa, 6 Oktober 2020, 17:46 WIB
+	formattedDate := TranslateDateToIndonesian(date)
+	assert.Equal(t, "Selasa, 6 Oktober 2020, 17:46 WIB", formattedDate)
 }
